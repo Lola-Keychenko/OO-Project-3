fb468f00cb553998374d01a16f4a107e50489b83
Lola Keychenko <lolakeychenko@engr2-25-49-dhcp.int.colorado.edu>
2022-09-29 10:48:51 -0600

    Working on treasure

---
+++ src/Sword.java
@@ -0,0 +1,8 @@
+public class Sword extends Treasure{
+
+    public Sword() {
+        type ="Sword";
+    }
+
+    //Sword does not need to know who has it, the adventurer does need to know if it has a sword
+}
