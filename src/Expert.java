9c520ccea9f2d6da38521a6a4a427c5c0f2aff02
Lola Keychenko <lolakeychenko@engr2-25-49-dhcp.int.colorado.edu>
2022-09-29 10:38:49 -0600

    update strat pattern

--- src/Expert.java
+++ src/Expert.java
@@ -1,4 +1,4 @@
-public class Expert implments CombatBehavior{
+public class Expert implements CombatBehavior{
     //Expert - gains +2 to 2 dice roll used for fighting a creature
     public int fight(Room R){
         return 1;
--- src/Trained.java
+++ src/Trained.java
@@ -1,4 +1,4 @@
-public class Trained implements combatBehavior{
+public class Trained implements CombatBehavior{
     //Trained - gains +1 to 2 dice roll for fighting a creature
     public int fight(Room r){
         return 1;
