a7a0d21601fe09879064ccedccec2e52d3f12172
Lola Keychenko <lolakeychenko@engr2-26-1-dhcp.int.colorado.edu>
2022-10-03 08:54:32 -0600

    Working on Treasure

---
+++ src/Gem.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Type;
+
+public class Gem extends Treasure{
+
+    public Gem(){
+        type = "Gem";
+    }
+
+    //cursed item, adventuer holding it gives +1 bonus to combat rolls by creatures against the adventurer
+}
---
+++ src/Trap.java
@@ -0,0 +1,8 @@
+public class Trap extends Treasure{
+
+    public Trap(){
+        type = "Trap";
+    }
+
+    //cause 1 point of damage to an adventurer
+}
