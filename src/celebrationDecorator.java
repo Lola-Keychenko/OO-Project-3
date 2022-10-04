31299c02409479c6e5f3cfa37630b450c90db74b
Lola-Keychenko <70180238+Lola-Keychenko@users.noreply.github.com>
2022-10-04 13:55:15 -0600

    Update celebrationDecorator.java

--- src/celebrationDecorator.java
+++ src/celebrationDecorator.java
@@ -1,3 +1,7 @@
+import java.util.Random;
+import java.util.*;
+import java.util.ArrayList;
 public abstract class celebrationDecorator extends Behavior{
-    public abstract String getCelebration();
+    //public abstract String getCelebration();
+    public abstract void fight(Room r);
 }
