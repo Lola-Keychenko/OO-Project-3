414e850cfae37f131b77628a1bf78a2d6fddfb1a
Lola-Keychenko <70180238+Lola-Keychenko@users.noreply.github.com>
2022-10-04 13:56:12 -0600

    Update Jump.java

--- src/Jump.java
+++ src/Jump.java
@@ -1,2 +1,13 @@
 public class Jump extends celebrationDecorator{
+    Behavior behavior; //instance variable to hold the behavior we are wrapping
+
+    public Jump(Behavior behavior){
+        this.behavior = behavior; //a way to set the instance variable to the object we are wrapping
+        //pass the behavior we are wrapping to the decorators constructor
+    }
+
+    public void fight(Room r){
+        //do extended behavior
+        System.out.println("Jump!");
+    }
 }
